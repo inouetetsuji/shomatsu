c4f0ac9b5ac831334b460aa9da40faa92acc980e
inouetetsuji <81937796+inouetetsuji@users.noreply.github.com>
2022-07-14 12:32:11 +0900

    Add files via upload

--- Check2/src/check/Pet.java
+++ Check2/src/check/Pet.java
@@ -1,37 +1,36 @@
 package check;
 
-
 public class Pet {
 
-    private String name;
-    private String masterName;
+	private String name;
+	private String masterName;
 
-    public Pet(String name, String masterName) {
-        this.name = name;
-        this.masterName = masterName;
-    }
-    
+	public Pet(String name, String masterName) {
+		this.name = name;
+		this.masterName = masterName;
+	}
+
 	protected String getName() {
-        return name;
-    }
+		return name;
+	}
 
-    protected String getMasterName() {
-        return masterName;
-    }
+	protected String getMasterName() {
+		return masterName;
+	}
 
-    public void introduce() {
-        System.out.println("■僕の名前は" + name + "です");
-        System.out.println("■ご主人様は" + masterName + "です");
-    }
+	public void introduce() {
+		System.out.println("■僕の名前は" + name + "です");
+		System.out.println("■ご主人様は" + masterName + "です");
+	}
 }
 
 class RobotPet extends Pet {
-    public RobotPet(String name, String masterName) {
-        super(name, masterName);
-    }
+	public RobotPet(String name, String masterName) {
+		super(name, masterName);
+	}
 
-    public void introduce() {
-        System.out.println("◇私はロボット。名前は" + getName() + "。");
-        System.out.println("◇ご主人様は" + getMasterName() + "。");
-    }
+	public void introduce() {
+		System.out.println("◇私はロボット。名前は" + getName() + "。");
+		System.out.println("◇ご主人様は" + getMasterName() + "。");
+	}
 }
